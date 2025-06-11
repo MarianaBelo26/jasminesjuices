b2c322eaa49874a0b94cd594bb527387ca23f0b7
Mariana Belo <101350371+MarianaBelo26@users.noreply.github.com>
2025-06-10 23:10:49 -0300

    update images

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -1,13 +1,13 @@
 export const products = [
-    { id: '1', name: 'Cashew Juice', image: '/jasminesjuices/juices_img/caju.png', price: 15, description: '100% natural cashew juice – sweet and slightly tangy, a unique tropical favorite.' },
-    { id: '2', name: 'Sugarcane Juice', image: '/jasminesjuices/juices_img/caldo_de_cana.png', price: 15, description: '100% natural sugarcane juice – naturally sweet and energizing' },
-    { id: '3', name: 'Dragon Fruit Juice', image: '/jasminesjuices/juices_img/pitaya.png', price: 15, description: '100% natural dragon fruit juice – exotic, refreshing, and rich in antioxidants.' },
-    { id: '4', name: 'Açaí Juice', image: '/jasminesjuices/juices_img/acai.png', price: 15, description: '100% natural açaí juice – powerful superfruit, rich in antioxidants and flavor.' },
-    { id: '5', name: 'Acerola and Orange Juice', image: '/jasminesjuices/juices_img/acerola_laranja.png', price: 15, description: '100% natural acerola and orange juice – a delicious combo, loaded with vitamin C.' },
-    { id: '6', name: 'Coconut Water', image: '/jasminesjuices/juices_img/agua_coco.png', price: 15, description: '100% natural coconut water – hydrating, light, and full of electrolytes.' },
-    { id: '7', name: 'Soursop Juice', image: '/jasminesjuices/juices_img/graviola.png', price: 15, description: '100% natural soursop juice – tropical flavor with a creamy texture, rich in nutrients.' },
-    { id: '8', name: 'Guaraná Juice', image: '/jasminesjuices/juices_img/guarana.png', price: 15, description: '100% natural guaraná juice – naturally stimulating and fruity, full of energy.' },
-    { id: '9', name: 'Orange Juice', image: '/jasminesjuices/juices_img/laranja.png', price: 15, description: '100% natural orange juice – classic, fresh, and packed with vitamin C.' },
-    { id: '10', name: 'Passion Fruit Juice', image: '/jasminesjuices/juices_img/maracuja.png', price: 15, description: '100% natural passion fruit juice – tangy, refreshing, and full of tropical vibes.' },
-    { id: '11', name: 'Green Juice (Kale, Apple, Pineapple & Honey)', image: '/jasminesjuices/juices_img/suco_verde.png', price: 15, description: '100% natural green juice – a revitalizing blend of kale, apple, pineapple, and honey.' }
+    { id: '1', name: 'Cashew Juice', image: '/juices_img/caju.png', price: 15, description: '100% natural cashew juice – sweet and slightly tangy, a unique tropical favorite.' },
+    { id: '2', name: 'Sugarcane Juice', image: '/juices_img/caldo_de_cana.png', price: 15, description: '100% natural sugarcane juice – naturally sweet and energizing' },
+    { id: '3', name: 'Dragon Fruit Juice', image: '/juices_img/pitaya.png', price: 15, description: '100% natural dragon fruit juice – exotic, refreshing, and rich in antioxidants.' },
+    { id: '4', name: 'Açaí Juice', image: '/juices_img/acai.png', price: 15, description: '100% natural açaí juice – powerful superfruit, rich in antioxidants and flavor.' },
+    { id: '5', name: 'Acerola and Orange Juice', image: '/juices_img/acerola_laranja.png', price: 15, description: '100% natural acerola and orange juice – a delicious combo, loaded with vitamin C.' },
+    { id: '6', name: 'Coconut Water', image: '/juices_img/agua_coco.png', price: 15, description: '100% natural coconut water – hydrating, light, and full of electrolytes.' },
+    { id: '7', name: 'Soursop Juice', image: '/juices_img/graviola.png', price: 15, description: '100% natural soursop juice – tropical flavor with a creamy texture, rich in nutrients.' },
+    { id: '8', name: 'Guaraná Juice', image: '/juices_img/guarana.png', price: 15, description: '100% natural guaraná juice – naturally stimulating and fruity, full of energy.' },
+    { id: '9', name: 'Orange Juice', image: '/juices_img/laranja.png', price: 15, description: '100% natural orange juice – classic, fresh, and packed with vitamin C.' },
+    { id: '10', name: 'Passion Fruit Juice', image: '/juices_img/maracuja.png', price: 15, description: '100% natural passion fruit juice – tangy, refreshing, and full of tropical vibes.' },
+    { id: '11', name: 'Green Juice (Kale, Apple, Pineapple & Honey)', image: '/juices_img/suco_verde.png', price: 15, description: '100% natural green juice – a revitalizing blend of kale, apple, pineapple, and honey.' }
   ]
