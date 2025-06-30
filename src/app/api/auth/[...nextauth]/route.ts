2fc3c8690acb3dccb452022de1c5aaf77ff743d6
Mariana Belo <101350371+MarianaBelo26@users.noreply.github.com>
2025-06-30 18:42:12 -0300

    update NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { Session , NextAuthOptions } from "next-auth"
 import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
